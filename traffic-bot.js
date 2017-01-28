4afd0409f079c278fe0b1045eeae14e3b12e6c47
cr7parker <romalezhnin@gmail.com>
2017-01-28 18:02:26 +0700

    Create backbone

--- traffic-bot.js
+++ traffic-bot.js
@@ -31,9 +31,9 @@ var S = require('./settings.json')
         pageSettings: {
             loadImages: true,        // The WebPage instance used by Casper will
             loadPlugins: true         // use these settings
-        }
-        //logLevel: "debug",              // Only "info" level messages will be logged
-        //verbose: true                  // log messages will be printed out to the console
+        },
+        logLevel: "debug",              // Only "info" level messages will be logged
+        verbose: true                  // log messages will be printed out to the console
     })
     , mouse = require("mouse").create(casper);
 //
@@ -69,7 +69,7 @@ casper.then(function () {
  Шаг 3 - притворяемся порядочным американцем ;-)
 */
 casper.then(function () {
-
+    PRETENDER_homoSapiens(this);
 });
 //
 //Bot start
@@ -78,8 +78,45 @@ casper.run(function () {
     this.echo('Casper TRAFFIC-GENERATOR run').exit();
 });
 //
+//
 // utils:
+//
 //
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+//
+// притворяемся человеком:
+//
+function PRETENDER_homoSapiens(self) {
+    _wait(self, 2);
+    self.scrollToBottom();
+    _wait(self, 2);
+    self.scrollTo(10, 0);
+    _wait(self, 5);
+    self.reload(function () { });
+    self.click('div > section:nth-child(' + (getRandomInt(1, 5) - 1) + ') iframe');
+    //
+    // go to youtube channel
+    //
+    self.waitForUrl(/www\.youtube\.com\/watch\?/, function () {
+        self.echo('PRETENDER_homoSapiens =GO= to Youtube')
+    });
+    self.back();
+    self.echo('PRETENDER_homoSapiens =RETURN= from Youtube');
+    //
+    // return to MFA site:
+    //
+    self.scrollToBottom();
+    _wait(self, 10);
+    self.scrollTo(300, 1000);
+    /*
+mouseup, mousedown, click, dblclick, mousemove, mouseover, mouseout , mouseenter, mouseleave , contextmenu
+    */
+    //this.sendKeys('body', 'f5', { modifiers: 'ctrl+alt' });
+
+}
+//
+_wait = function (self, sec) {
+    return self.wait(sec * 1000, function () { });
+}
